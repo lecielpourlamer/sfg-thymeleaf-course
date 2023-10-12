802b78f9dcfeb73f7fceaa2eb43930f0e23a8bdb
lecielpourlamer <shlim20190902@gmail.com>
2023-10-12 13:41:35 +0900

    Initial commit

---
+++ src/main/java/ocean/springframework/sfgthymeleafcourse/SfgThymeleafCourseApplication.java
@@ -0,0 +1,13 @@
+package ocean.springframework.sfgthymeleafcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgThymeleafCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgThymeleafCourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ocean/springframework/sfgthymeleafcourse/SfgThymeleafCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package ocean.springframework.sfgthymeleafcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgThymeleafCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
